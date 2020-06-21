97e64fa0f57911525036ddf32cd516e4e9c52adf
difficulttochoose <pmd27041997@gmail.com>
2020-06-21 17:10:40 +0300

    Add if-else tasks to index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+//IF-ELSE----------------------------------------------------------------------------------
+
+//1
+const a = Number(prompt('Enter your number: 1, 0 or -3'));
+if (a === 0) {
+    alert('Right');
+} else {
+    alert('Wrong');
+}
+
+//2
+const a2 = Number(prompt('Enter your number: 1, 0 or -3'));
+if (a2 > 0) {
+    alert('Right');
+} else {
+    alert('Wrong');
+}
+
+//3
+const a3 = Number(prompt('Enter your number: 1, 0 or -3'));
+if (a3 < 0) {
+    alert('Right');
+} else {
+    alert('Wrong');
+}
+
+//4
+const a4 = Number(prompt('Enter your number: 1, 0 or -3'));
+if (a4 >= 0) {
+    alert('Right');
+} else {
+    alert('Wrong');
+}
+
+//5
+const a5 = Number(prompt('Enter your number: 1, 0 or -3'));
+if (a5 <= 0) {
+    alert('Right');
+} else {
+    alert('Wrong');
+}
+
+//6
+const a6 = Number(prompt('Enter your number: 1, 0 or -3'));
+if (a6 !== 0) {
+    alert('Right');
+} else {
+    alert('Wrong');
+}
+
+//7
+const a7 = prompt('Enter your value: test, тест, 3');
+if (a7 === 'test') {
+    alert('Right');
+} else {
+    alert('Wrong');
+}
+
+//8
+let a8 = '1';//check with '1', 1, 3
+if (a8 === '1') {
+    alert('Right');
+} else {
+    alert('Wrong');
+}
