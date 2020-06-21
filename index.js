ddc9774a706bdf7fda76c4b32c656e1b3674f566
difficulttochoose <pmd27041997@gmail.com>
2020-06-21 17:14:35 +0300

    Add switch-case tasks to index.js

--- index.js
+++ index.js
@@ -104,3 +104,116 @@ if (r > 2 && r < 11 || t >= 6 && t < 14) {
 } else {
     alert('Wrong');
 }
+
+
+//SWITCH-CASE--------------------------------------------------------------------------------------
+
+//1
+const num = Number(prompt('Enter your number: 1, 2, 3, 4'));
+let result;
+switch (num) {
+    case 1: {
+        result = 'зима';
+    }
+        break;
+    case 2: {
+        result = 'весна';
+    }
+        break;
+    case 3: {
+        result = 'лето';
+    }
+        break;
+    case 4: {
+        result = 'осень';
+    }
+        break;
+    default: {
+        result = 'ОШИБКА';
+    }
+}
+alert(result);
+
+//2
+const day = Number(prompt('Enter a day of the month from 1 to 31'));
+let decade;
+switch (day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10: {
+        decade = 'Your number is in the first decade of the month';
+    }
+        break;
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+    case 20: {
+        decade = 'Your number is in the second decade of the month';
+    }
+        break;
+    case 21:
+    case 22:
+    case 23:
+    case 24:
+    case 25:
+    case 26:
+    case 27:
+    case 28:
+    case 29:
+    case 30:
+    case 31: {
+        decade = 'Your number is in the third decade of the month';
+    }
+        break;
+    default: {
+        decade = 'ERROR! Maximum 31 days in a month';
+    }
+}
+alert(decade);
+
+//3
+const month = Number(prompt('Enter a number from 1 to 12'))
+let timeOfTheYear;
+switch (month) {
+    case 1:
+    case 2:
+    case 3: {
+        timeOfTheYear = 'winter';
+    }
+        break;
+    case 4:
+    case 5:
+    case 6: {
+        timeOfTheYear = 'spring';
+    }
+        break;
+    case 7:
+    case 8:
+    case 9: {
+        timeOfTheYear = 'summer';
+    }
+        break;
+    case 10:
+    case 11:
+    case 12: {
+        timeOfTheYear = 'autumn';
+    }
+        break;
+    default: {
+        timeOfTheYear = 'There are only four times of the year';
+    }
+}
+alert(timeOfTheYear);
